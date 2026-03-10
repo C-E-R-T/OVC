eb351b05784212fe7fc214146d1242c2c55a53c2
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-10 16:02:45 +0900

    axios -> try-catch문 추가

--- src/api/certificate.ts
+++ src/api/certificate.ts
@@ -2,6 +2,11 @@ import type { Certificate } from "../types/exam";
 import { apiClient } from "./apiClient";
 
 export async function getCertificates(cert_id: number): Promise<Certificate> {
-    const res = await apiClient.get(`/certs/${cert_id}`);
-    return res.data.data;
+    try {
+        const res = await apiClient.get(`/certs/${cert_id}`);
+        return res.data.data;
+    } catch (error) {
+        console.error("certificate 조회 실패", error);
+        throw error;
+    }
 }
