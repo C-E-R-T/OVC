9910d841b86c0824ed199d3c7bd3fb96d74ab7fc
dayoung0311 <dyl0311@gachon.ac.kr>
2026-03-11 14:19:30 +0900

    axios 파일 정리 및 env 파일 추가

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,9 +1,22 @@
+//axios 공통 설정 파일
 import axios from "axios";
 
+//axios 인스턴스 생성
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+//요청을 보내기 전에 실행됨
+apiClient.interceptors.request.use((config)=> {
+  const token =localStorage.getItem("accessToken");
+
+  if(token){
+    config.headers.Authorization=`Bearer ${token}`;
+  }
+
+  return config;
+})
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,16 +1,12 @@
 // 로그인/회원가입에 대한 API 호출 분리
-import axios from "axios";
-
-const API=axios.create({
-    baseURL: "http://localhost:8080/api"
-});
+import { apiClient } from "./apiClient";
 
 export const signup=async(data: {
     email: string;
     password: string;
     userName: string;
 })=> {
-    const res=await API.post("auth/signup",data);
+    const res=await apiClient.post("/auth/signup",data);
     return res.data.data;
 };
 
@@ -18,6 +14,6 @@ export const login =async(data : {
     email: string;
     password: string;
 })=> {
-    const res=await API.post("auth/login",data);
+    const res=await apiClient.post("/auth/login",data);
     return res.data.data;
 }
